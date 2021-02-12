f6e827a0d54b66e7d89bb2f9547928e88b9c2ff9
sjatar <sjatar@paypal.com>
2021-02-12 12:44:11 -0800

    Posts complete

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,7 +12,7 @@ const { check, validationResult } = require('express-validator');
 // @access  Public
 router.get('/', auth, async (req, res) => {
     try {
-      const user = await User.findById(req.user.id).select('-password');
+      const user = await User.findById(req.user.id).select('-password'); // so that password is not returned in response
       res.json(user);
     } catch (err) {
       console.error(err.message);
